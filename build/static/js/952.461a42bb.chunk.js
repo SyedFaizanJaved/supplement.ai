ae8cae998bff8a0b5356af44f4c6a7cca828ac3d
manshaoceans <mansha.aazar@neurooceans.ai>
2025-02-18 15:35:10 +0500

    .gitignore added

---
+++ build/static/js/952.461a42bb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknutrea_genie=self.webpackChunknutrea_genie||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.461a42bb.chunk.js.map
